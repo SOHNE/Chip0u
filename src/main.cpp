2d3dacfa40bc1512303cf267649f1d4cce2e6c98
Leandro Peres <leandroperes@protonmail.com>
2024-02-14 17:31:11 -0300

    Application loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,44 @@
-#include <iostream>
+#include "app/Application.h"
+
+#if __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+void
+AppLoop(void* arg) {
+    Application& app = *reinterpret_cast<Application*>(arg);
+
+    if (!app.IsRunning()) {
+#if __EMSCRIPTEN__
+        emscripten_cancel_main_loop();
+#endif
+        return;
+    }
+
+    app.Input();
+    app.Update();
+    app.Render();
+}
 
 int
-main()
+main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Silence unused variable warnings
+    (void)argc;
+    (void)argv;
+
+    Application app;
+    app.Setup();
+
+#if __EMSCRIPTEN__
+    emscripten_set_main_loop_arg(AppLoop, &app, 0, 1);
+#else
+    while (app.IsRunning()) {
+        AppLoop(&app);
+    }
+#endif
+
+    app.Destroy();
+
     return 0;
 }
